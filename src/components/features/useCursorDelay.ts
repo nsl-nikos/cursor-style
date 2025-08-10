9bff671d64b96dfb6f87b65ec97f6a2339c33143
nsl-nikos <nikosmurariu@gmail.com>
2025-08-10 21:29:51 +0300

    alpha factor for future

--- src/components/features/useCursorDelay.ts
+++ src/components/features/useCursorDelay.ts
@@ -26,7 +26,8 @@ export const useCursorDelay = (delay: number = 0, initialPosition: Position) => 
     if (delay === 0) return;
 
     const updatePosition = () => {
-      const alpha = 1 - delay * 0.09;
+      const ALPHA_FACTOR = 0.09; // I will need this in the future
+      const alpha = 1 - delay * ALPHA_FACTOR;
       const newX = (1 - alpha) * position.x + alpha * targetPosition.current.x;
       const newY = (1 - alpha) * position.y + alpha * targetPosition.current.y;
       
